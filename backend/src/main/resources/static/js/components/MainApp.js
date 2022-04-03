cb4bf28c49150afbd97bb629447c9a011638d1b0
Feras Wilson <feras.wilson@tietoevry.com>
2022-04-03 13:27:51 +0200

    Update version

--- backend/src/main/resources/static/js/components/MainApp.js
+++ backend/src/main/resources/static/js/components/MainApp.js
@@ -582,7 +582,7 @@ class MainApp extends React.Component {
                                         <span><img src="images/logo.png" alt=""
                                         /></span>
                                 </div>
-                                <div className="text-center"><small>Version: 0.14.2</small></div>
+                                <div className="text-center"><small>Version: 0.14.3</small></div>
                                 <ul className="sidebar-nav nav">
 
                                     <li><a href="#" onClick={this.newWorkflow}><i className="fas fa-file"></i> New task</a>
